863c4191a8caf2188c1af93a7e04cb3f86428f0c
Huadong <sakimarquis@gmail.com>
2026-04-27 12:22:24 -0700

    feat(llm): add v2 backend contracts

---
+++ ruff_cm/llm/__init__.py
@@ -0,0 +1,1 @@
+"""Reusable LLM research primitives."""
---
+++ ruff_cm/llm/backends/__init__.py
@@ -0,0 +1,12 @@
+from .base import BackendCapabilityError, CaptureResult, ChoiceScores, GenerateResult, Generator, HiddenReader, Message, Scorer
+
+__all__ = [
+    "BackendCapabilityError",
+    "CaptureResult",
+    "ChoiceScores",
+    "GenerateResult",
+    "Generator",
+    "HiddenReader",
+    "Message",
+    "Scorer",
+]
