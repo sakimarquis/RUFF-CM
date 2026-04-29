9df59d3010261493306f152a3d4ffe6f997dc88b
Huadong <sakimarquis@gmail.com>
2026-04-28 20:07:18 -0700

    Restore package eager exports

--- ruff_cm/__init__.py
+++ ruff_cm/__init__.py
@@ -1,10 +1,1 @@
-import importlib
-
-
-__all__ = ["experimenter", "logger", "plotter", "utils"]
-
-
-def __getattr__(name):
-    if name in __all__:
-        return importlib.import_module(f"{__name__}.{name}")
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+from ruff_cm import experimenter, logger, plotter, utils
